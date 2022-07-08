ba2959d8ebb820eef9ba743aa5061dda247eb0fa
abhishekdhiman9 <abhishek.dhiman@antiersolutions.com>
2022-07-08 18:24:41 +0530

    v3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
+import Web3 from 'web3';
 
 const App = () => {
+
   const [walletConnected, setWalletConnected] = useState(false);
   const [instruction, setInstruction] = useState("waiting for connection with wallet");
+
+  useEffect(() => {
+    const connectWallet = async() => {
+      if(!window.ethereum)
+       return;
+  
+       try {
+        await window.ethereum.send("eth_requestAccounts");
+        window.web3 = new Web3(window.ethereum);
+       } catch (error) {
+        setInstruction("Wallet connection denied, reload the page to try again!! ");
+        return;
+       }
+       setInstruction("");
+       setWalletConnected(true);
+    }
+    connectWallet();
+  }, []);
 
   return (
     <div>
