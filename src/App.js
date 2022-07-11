e9b027422e6ea36688054c5ee38793e11f28bebe
abhishekdhiman9 <abhishek.dhiman@antiersolutions.com>
2022-07-11 16:33:10 +0530

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import React from 'react';
-import { useState, useEffect } from "react";
-import Web3 from 'web3';
-import AppAuthenticated from './components/AppAuthenticated';
+import { useState, useEffect } from 'react'
+import AppAuthenticated from "./components/AppAuthenticated"
+const Web3 = require("web3");
 
 const App = () => {
-
   const [walletConnected, setWalletConnected] = useState(false);
-  const [instruction, setInstruction] = useState("waiting for connection with wallet");
+  const [instruction, setInstruction] = useState("Waiting for connection with wallet...");
 
   useEffect(() => {
-    const connectWallet = async() => {
+    const connectWallet = async () => {
       if(!window.ethereum)
-       return;
-  
-       try {
-        await window.ethereum.send("eth_requestAccounts");
-        window.web3 = new Web3(window.ethereum);
-       } catch (error) {
-        setInstruction("Wallet connection denied, reload the page to try again!! ");
         return;
-       }
-       setInstruction("");
-       setWalletConnected(true);
-    }
+
+      try {
+        await window.ethereum.send('eth_requestAccounts');
+        window.web3 = new Web3(window.ethereum);
+      } catch (error) {
+        setInstruction("Wallet connection denied, reload the page to try again.");
+        return;
+      }
+      setInstruction("");
+      setWalletConnected(true);
+    };
     connectWallet();
   }, []);
 
   return (
     <div>
-      { window.ethereum ? 
-        (walletConnected ? 
-        <AppAuthenticated />
-         : instruction
-         ) : "Metamast or other EIP-1102/EIP-1193 complian wallet not found !"
-    }
+      {window.ethereum ?
+        (walletConnected ?
+          <AppAuthenticated/>
+          : instruction)
+        : "Metamask or other EIP-1102 / EIP-1193 compliant wallet not found."
+      }
     </div>
   )
 }
 
-export default App;
+export default App
